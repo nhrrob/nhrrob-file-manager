44c4c3ec93b312154521e8e67335e58ee2b86c77
Nazmul Hasan Robin <robin.sust08@gmail.com>
2026-01-05 11:33:31 +0600

    feat: Add Composer autoloader files and update .gitignore.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nhrfm/file-manager',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a0b5ab88148c5ee3142d3d59bd9ca9cb049042a2',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'nhrfm/file-manager' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a0b5ab88148c5ee3142d3d59bd9ca9cb049042a2',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
